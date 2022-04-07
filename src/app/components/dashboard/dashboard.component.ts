6f356fbf0fc9beaae40868fb2efad68a21c71c47
Joris <joris.martin@etu.univ-grenoble-alpes.fr>
2022-04-07 11:36:56 +0200

    Ajout armes dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,27 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { IHero } from '../../interfaces/hero.interface';
-import { HeroService } from '../../services/entity/hero.service';
-
+import {Component, OnInit} from '@angular/core';
+import {IHero} from '../../interfaces/hero.interface';
+import {HeroService} from '../../services/entity/hero.service';
+import {WeaponService} from "../../services/entity/weapon.service";
+import {IWeapon} from "../../interfaces/weapon.interface";
 
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.scss' ]
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  TOP_HEROES_NUMBER: number = 5;
-  heroes: IHero[] = [];
+	TOP_NUMBER: number = 5;
+	heroes: IHero[] = [];
+	weapons: IWeapon[] = [];
 
-  constructor(private heroService: HeroService) { }
+	constructor(
+		private heroService: HeroService,
+		private weaponService: WeaponService,
+	) {}
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+	ngOnInit(): void {
+		this.getHeroes();
+		this.getWeapons();
+	}
 
-  getHeroes(): void {
-    this.heroService.getAll()
-      .subscribe(heroes => this.heroes = heroes.slice(0, this.TOP_HEROES_NUMBER - 1));
-  }
+	getHeroes(): void {
+		this.heroService.getAll()
+			.subscribe(heroes => this.heroes = heroes.slice(0, this.TOP_NUMBER - 1));
+	}
+
+	getWeapons(): void {
+		this.weaponService.getAll()
+			.subscribe(weapon => this.weapons = weapon.slice(0, this.TOP_NUMBER - 1));
+	}
 }
